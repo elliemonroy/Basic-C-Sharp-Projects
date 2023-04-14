df617030e700f0607e8170f6a1cca4e843a1ba00
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-14 15:23:28 -0700

    Boolean Comparison While Statement

---
+++ ConsoleApplicationAssignment/BooleanComparisonWhileStatement/BooleanComparisonWhileStatement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooleanComparisonWhileStatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Continue while-loop until index is equal to 10
+            int i = 0;
+            while (i < 10)
+            {
+                // Display the index to the console
+                Console.Write("While statement ");
+                Console.WriteLine(i);
+                i++;
+            }
+        }
+    }
+}
