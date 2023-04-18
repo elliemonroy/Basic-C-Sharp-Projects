2aa4f89fe53e52bc8d9aebbce900b08098687d93
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-18 15:20:30 -0700

    Method Class Assignment

---
+++ MethodClass/MethodClass/Problems.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    public class Problems
+    {
+        // Created a void method that takes two integers
+        public void Problem(int first, int second)
+        {
+            // This will do math operation on the first integer and display second integer on the console
+            int number = first + 5;
+            Console.WriteLine(second);
+        }
+    }
+}
---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating the class Problems
+            Problems problems = new Problems();
+            // Calling Problem method and passing in two numbers
+            problems.Problem(10, 24);
+            // Calling Problem method, specifying the parameters by name and giving them values
+            problems.Problem(first: 2, second: 1);
+
+            Console.ReadLine();
+        }
+    }
+}
