a09d5911a13bcd7ee7113693c5c1ef8f8a8be62f
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-18 10:06:46 -0700

    Calling Methods Assignment

---
+++ CallingMethods/CallingMethods/Math.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    public class Math
+    {
+        // Method that takes user input and adds it to 300
+        public int Add(int userValue)
+        {
+            int answer = userValue + 300;
+            return answer;
+        }
+        // Method that takes user input and subtracts it by 11
+        public int Subtract(int userValue)
+        {
+            int answer = userValue - 11;
+            return answer;
+        }
+        // Method that takes user input and multiplies it by 10
+        public int Multiply(int userValue)
+        {
+            int answer = userValue * 10;
+            return answer;
+        }
+    }
+}
---
+++ CallingMethods/CallingMethods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This will ask user to input a number on the console
+            Console.WriteLine("Enter a number you want to do a math operation on:");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            // This calls the class Math containing three methods
+            Math math = new Math();
+
+            // This will print all three math problems and their answers to the console
+            Console.WriteLine(userValue + " + 300 = " + math.Add(userValue));
+            Console.ReadLine();
+
+            Console.WriteLine(userValue + " - 11 = " + math.Subtract(userValue));
+            Console.ReadLine();
+
+            Console.WriteLine(userValue + " * 10 = " + math.Multiply(userValue));
+            Console.ReadLine();
+
+        }
+    }
+} 
