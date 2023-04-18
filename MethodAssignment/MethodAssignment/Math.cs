a71a8716ccf3dd18ccea8dd995a697566adb4802
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-18 14:40:58 -0700

    Method Assignment

---
+++ MethodAssignment/MethodAssignment/Math.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    public class Math
+    {
+        // This takes two integers, if it does not use 12
+        public int Integer(int numOne, int numTwo = 12)
+        {
+            // Adds the two integers
+            return numOne + numTwo;
+        }
+    }
+}
---
+++ MethodAssignment/MethodAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This calls the class Math that contains the method
+            Math math = new Math();
+
+            // This will ask user to enter a number 
+            Console.WriteLine("Please enter a number:");
+            int numOne = Convert.ToInt32(Console.Read());
+
+            // This will ask user to enter a second number or leave blank 
+            Console.WriteLine("Please enter a second number (This is optional, press enter to leave blank):");
+            string strNum = Console.ReadLine();
+            
+
+            // If the second number is left blank, do this 
+            if (strNum == "")
+            {
+                Console.WriteLine(numOne + " + 12 = " + math.Integer(numOne));
+                Console.ReadLine();
+            }
+            // If there are two numbers entered, do this 
+            else
+            {
+                int numTwo = Convert.ToInt32(strNum);
+                Console.WriteLine(numOne + " + " + numTwo + " = " + math.Integer(numOne, numTwo));
+                Console.ReadLine();
+            }
+        }
+    }
+}
