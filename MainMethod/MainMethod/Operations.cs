338442407746a23f785c1b782ead68f427553b94
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-18 11:53:22 -0700

    Main Method Assignment

---
+++ MainMethod/MainMethod/Operations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Operations
+    {
+        // This will take in an integer and return as an integer
+        public int Operation(int answer)
+        {
+            return answer + 225;
+        }
+        // This will take in a decimal and return the answer as an integer
+        public int Operation(decimal decimalNum)
+        {
+            int answer = Convert.ToInt32(decimalNum) * 12;
+            return answer;
+        }
+        // This will take in a string and convert into an integer
+        public int Operation(string number)
+        {
+            int answer = Convert.ToInt32(number) - 1;
+            return answer;
+        }
+    }
+}
---
+++ MainMethod/MainMethod/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calls the class Operations that contains the methods
+            Operations operations = new Operations();
+
+            // This calls my first method and passes an integer to the equation
+            int answer = operations.Operation(30);
+            Console.WriteLine("30 + 225 = " + answer);
+            Console.ReadLine();
+
+            // This calls my second method that takes in a decimal and returns an integer
+            int answer2 = operations.Operation(22.2m);
+            Console.WriteLine("22.2 * 12 = " + answer2);
+            Console.ReadLine();
+
+            // This calls my third method that takes in a string and returns an integer
+            int answer3 = operations.Operation("5");
+            Console.WriteLine("\"5\" - 1 = " + answer3);
+            Console.ReadLine();
+
+        }
+    }
+}
