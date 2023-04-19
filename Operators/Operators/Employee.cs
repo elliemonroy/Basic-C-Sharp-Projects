73d3be7109085791ca88be1ed2dfc7ae8a75b3a3
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-19 12:50:31 -0700

    Operators Assignment

---
+++ Operators/Operators/Employee.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    // Created class Employee
+    public class Employee
+
+    {
+        // Gave it three properties Id, FirstName, and LastName
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        // This will check if employee1 Id is equal to employee2 Id
+        public static bool operator == (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id == employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        // This will check if employee1.Id is not equal to employee2.Id
+        public static bool operator != (Employee employee1, Employee employee2)
+        {
+            if (employee1.Id != employee2.Id)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiating two objects of the Employee class and assigned values to their properties
+            Employee employee = new Employee() { FirstName = "Bob", LastName = "Simmons", Id = 1111 };
+            Employee employee1 = new Employee() { FirstName = "Carey", LastName = "Jones", Id = 1111 };
+
+            // This will print their information to the console
+            Console.WriteLine(employee.FirstName + " " + employee.LastName + " " + employee.Id);
+            Console.WriteLine(employee1.FirstName + " " + employee1.LastName + " " + employee1.Id);
+            Console.ReadLine();
+
+            // If employee.Id is == employee1.Id print this message to the console 
+            if(employee == employee1)
+            {
+                Console.WriteLine("The Ids of the two employees match.");
+            }
+            // if not print this message to the console 
+            else
+            {
+                Console.WriteLine("The Ids are unique.");
+            }
+        }
+    }
+}
