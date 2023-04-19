f69415eb75f63975691e97ee4d2b481d53051454
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-19 16:06:58 -0700

    Struct Assignment

---
+++ Struct/Struct/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Struct
+{
+    class Program
+    {
+        // Created a struct called number 
+        public struct Number
+        {
+            // Gave it the property amount and made the data type be decimal 
+            public decimal Amount; 
+        }
+        static void Main(string[] args)
+        {
+            // Created an object of data type Number and assigned an amount 
+            Number number = new Number();
+            number.Amount = 150.24m;
+
+            // This will display the amount to the console 
+            Console.WriteLine("This is the amount: " + number.Amount);
+            Console.ReadLine();
+
+
+        }
+    }
+}
