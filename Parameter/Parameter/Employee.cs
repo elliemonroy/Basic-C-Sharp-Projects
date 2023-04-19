c7c39f07a50b1a92e3db56eb4f028e3748a69b79
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-19 14:18:02 -0700

    Parameters Assignment

---
+++ Parameter/Parameter/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    // Generic parameter
+    public class Employee <T>
+    {
+        // Added a property called things 
+        public List<T> things { get; set; }
+    }
+}
---
+++ Parameter/Parameter/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiated Employee object with type string and assigned it a list of names
+            Employee<string> name = new Employee<string>();
+            name.things = new List<string> { "Ellie", "Joe", "Drako" };
+
+            // Created a loop to display all the names on the list to the console
+            foreach(string thing in name.things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            // Instantiated Employee object with type int and assigned it a list of numbers
+            Employee<int> number = new Employee<int>();
+            number.things = new List<int> { 10, 24, 1993 };
+
+            // Created a loop to display all the numbers on the list to the console
+            foreach(int thing in number.things)
+            {
+                Console.WriteLine(thing);
+            }
+            Console.ReadLine();
+        }
+    }
+}
