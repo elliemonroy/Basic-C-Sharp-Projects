5f82883e2345a5cde47403abd05dda4fdc6377de
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-21 10:44:28 -0700

    Constructor Assignment

---
+++ ConstructorAssignment/ConstructorAssignment/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    public class Person
+    {
+        // Created second constructor that inherits from first one and takes in one parameter
+        public Person(string name) : this(name, 28)
+        {
+
+        }
+        // Created first constructor  
+        public Person(string name, int howOld)
+        {
+            Name = name;
+            Age = howOld;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Created a const variable 
+            const string color = "pink";
+
+            // Created variable using the keyword var
+            var sentence = "My favorite color is";
+            // Printed full senence to the console 
+            Console.WriteLine("Hi, my name is Ellie. {0} {1}.", sentence, color);
+
+            // Created new person by chaining two constructors together 
+            Person newPerson = new Person("Sam");
+
+            Console.WriteLine(newPerson.Name +" is "+ newPerson.Age +" years old.");
+            Console.ReadLine();
+
+        }
+    }
+}
