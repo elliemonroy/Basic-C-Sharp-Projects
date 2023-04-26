f442d6f3032ab20d40954da3f96f667c3fe76ac3
elliemonroy <122836246+elliemonroy@users.noreply.github.com>
2023-04-26 11:37:29 -0700

    Code First Application Assignment

---
+++ CodeFirstApp/CodeFirstApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Data.Entity;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp
+{
+    class Program
+    {
+        public static object FiddleHelper { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // This will add new student Ken to the database
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Ken" };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+
+                Console.Write("Student saved successfully!");
+                Console.ReadLine();
+
+
+     
+            }
+        }
+    }
+}
---
+++ CodeFirstApp/CodeFirstApp/SchoolContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeFirstApp
+{
+    // This is required so it can inherit from the DbContext class so we can use DbSet properties for our model
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext(): base()
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+    }
+}
